bcac0a70c7ec42036e24b3952e546bae63f4686d
bsc-com-15-24 <bsc-com-15-24@gmail.com>
2026-04-29 09:29:12 +0200

    auth and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { AuthModule } from './auth/auth.module';
+import { Auth#Controller } from './auth#/auth#.controller';
 
 @Module({
-  imports: [StudentsModule],
-  controllers: [AppController],
+  imports: [StudentsModule, AuthModule],
+  controllers: [AppController, Auth#Controller],
   providers: [AppService],
 })
 export class AppModule {}
