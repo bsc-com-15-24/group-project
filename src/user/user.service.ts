482e7458eb9ce8c33ed7a39e00c92a09d11cf0cb
bsc-com-15-24 <bsc-com-15-24@gmail.com>
2026-04-30 08:47:25 +0200

    stuff

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -24,30 +24,4 @@ export class UserService {
   }
 }
 
-import { Controller, Get, Param, Put, Delete, Body } from '@nestjs/common';
-import { UsersService } from './users.service';
 
-@Controller('api/v1/users')
-export class UsersController {
-  constructor(private usersService: UsersService) {}
-
-  @Get()
-  getAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  getOne(@Param('id') id: number) {
-    return this.usersService.findOne(id);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: number, @Body() body: any) {
-    return this.usersService.update(id, body);
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: number) {
-    return this.usersService.remove(id);
-  }
-}
