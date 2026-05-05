c5631f7182723203d21b7523655f08a1294a88ce
bsc-com-15-24 <bsc-com-15-24@gmail.com>
2026-05-05 03:07:04 +0200

    registering enabled

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-auth-dto';
+import { RegisterDto } from './dto/register-auth-dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     @Post('login')
     async login(@Body() loginDto: LoginDto) {
-        // We now pass the DTO directly to the service to verify the password!
         return this.authService.login(loginDto);
+    }
+
+    @Post('register')
+    async register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../user/user.service';
 import { LoginDto } from './dto/login-auth-dto';
+import { RegisterDto } from './dto/register-auth-dto';
 
 @Injectable()
 export class AuthService {
@@ -22,6 +23,28 @@ export class AuthService {
         if (user.password !== loginDto.password) {
             throw new UnauthorizedException('Invalid credentials');
         }
+
+        // 3. Create payload and generate token
+        const payload = { email: user.email, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+
+    async register(registerDto: RegisterDto) {
+        // 1. Check if user already exists
+        const existingUser = await this.usersService.findByEmail(registerDto.email);
+        if (existingUser) {
+            throw new UnauthorizedException('User with this email already exists');
+        }
+
+        // 2. create user
+        const user = await this.usersService.create({
+            name: registerDto.username,
+            email: registerDto.email,
+            password: registerDto.password,
+            role: registerDto.role,
+        });
 
         // 3. Create payload and generate token
         const payload = { email: user.email, sub: user.id };
