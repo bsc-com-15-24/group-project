7a9aa2d93e290e1f08cd169c8f35dda117c36e10
bsc-com-15-24 <bsc-com-15-24@gmail.com>
2026-05-05 03:11:10 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './user/user-entity';
@@ -11,26 +8,26 @@ import { ResourcesModule } from './resources/resources.module';
 
 
 @Module({
-imports: [
-ConfigModule.forRoot({ isGlobal: true }),
-TypeOrmModule.forRootAsync({
-imports: [ConfigModule],
-inject: [ConfigService],
-useFactory: (config: ConfigService) => ({
-type: 'oracle',
-host: config.get('DB_HOST'),
-port: parseInt(config.get<string>('DB_PORT')!),
-username: config.get('DB_USERNAME'),
-password: config.get('DB_PASSWORD'),
-serviceName: config.get('DB_SERVICE_NAME'),
-synchronize: config.get('DB_SYNCHRONIZE') === 'true',
-entities: [User],
-logging: true,
-}),
-}),
-AuthModule,
-UserModule,
-ResourcesModule,
-],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'oracle',
+                host: config.get('DB_HOST'),
+                port: parseInt(config.get<string>('DB_PORT')!),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                serviceName: config.get('DB_SERVICE_NAME'),
+                synchronize: config.get('DB_SYNCHRONIZE') === 'true',
+                entities: [User],
+                logging: true,
+            }),
+        }),
+        AuthModule,
+        UserModule,
+        ResourcesModule,
+    ],
 })
-export class AppModule {}
+export class AppModule { }
