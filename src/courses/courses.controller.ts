f016c78f5344a04202f352a056308bed5e52b560
bsc-com-15-24 <bsc-com-15-24@gmail.com>
2026-05-07 05:40:04 +0200

    .

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Body,
-  Param,
-  ParseIntPipe,
-} from '@nestjs/common';
+import {Controller,Get,Post,Put,Delete,Body,Param,ParseIntPipe,} from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { Course } from './course.entity';
 import { CreateCourseDto } from './dto/create-course.dto';
